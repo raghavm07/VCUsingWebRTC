86853fb69188970aeb22d1bc72012948cc99b2b1
MIND\raghav.mohan <raghav.mohan@motherson.com>
2023-12-25 10:37:14 +0530

    vercel.json update

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -1,35 +1,3 @@
-// const { Server } = require("socket.io");
-// const io = new Server(8000, {
-//   cors: true,
-// });
-// const emailToSocketIdMap = new Map();
-// const socketToEmailIdMap = new Map();
-
-// io.on("connection", (socket) => {
-//   console.log(`Socket Connected`, socket.id);
-//   socket.on("room:join", (data) => {
-//     //console.log(data);
-//     const { email, roomCode } = data;
-//     emailToSocketIdMap.set(email, socket.id);
-//     socketToEmailIdMap.set(socket.id, email);
-//     io.to(roomCode).emit("user:joined", { email, id: socket.id });
-//     socket.join(roomCode);
-//     io.to(socket.id).emit("room:join", data);
-//   });
-
-//   socket.on("user:call", ({ to, offer }) => {
-//     io.to(to).emit("incoming:call", { from: socket.id, offer });
-//   });
-//   socket.on("call:accepted", ({ to, ans }) => {
-//     io.to(to).emit("call:accepted", { from: socket.id, ans });
-//   });
-//   socket.on("peer:nego:needed", ({ to, offer }) => {
-//     io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
-//   });
-//   socket.on("peer:nego:done", ({ to, ans }) => {
-//     io.to(to).emit("peer:nego:final", { from: socket.id, ans });
-//   });
-// });
 const { Server } = require("socket.io");
 
 const io = new Server(8000, {
